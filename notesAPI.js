4fed0566fa46ca92afed177bed5db4d147068d32
Vitali Schumarov <v.schumarov@icloud.com>
2025-01-09 21:29:09 +0100

    done

---
+++ notesAPI.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", (event) => {
+  removeElementsByClass();
+  loadDataFromAPI();
+});
+
+function loadDataFromAPI() {
+  const rawData = localStorage.getItem("allNotesData");
+  if (rawData) {
+    const data = JSON.parse(rawData);
+    allMyNotes = data.sort((a, b) => b.timestamp - a.timestamp);
+    console.log(allMyNotes);
+    for (let i = 0; i < allMyNotes.length; i++) {
+      let date = new Date(allMyNotes[i].timestamp);
+      let formattedDate = date.toLocaleString("de-DE");
+      console.log(date);
+      create_note_UI(
+        allMyNotes[i].title,
+        allMyNotes[i].note_message,
+        formattedDate,
+        allMyNotes[i].id
+      );
+    }
+  } else {
+    console.log(`data not loaded`);
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("allNotesData", JSON.stringify(allMyNotes));
+  clearInputFields();
+}
---
+++ script.js
@@ -0,0 +1,138 @@
+let allMyNotes = [];
+let selectedNote = false;
+let selectedID = "";
+
+function create_new_note(inputTitle, inputNote) {
+  const idCreated = Math.random();
+  const timeStamp = Date.now();
+  const note = {
+    id: idCreated,
+    title: inputTitle,
+    note_message: inputNote,
+    timestamp: timeStamp,
+  };
+  allMyNotes.push(note);
+  saveToLocalStorage();
+  removeElementsByClass();
+  loadDataFromAPI();
+  // create_note_UI(inputTitle, inputNote, timeStamp, idCreated);
+}
+
+function save_note() {
+  if (
+    document.getElementById("title-input-field").value != "" &&
+    document.getElementById("textArea-input-field").value != ""
+  ) {
+    if (selectedNote === false) {
+      create_new_note(
+        document.getElementById("title-input-field").value,
+        document.getElementById("textArea-input-field").value
+      );
+      return;
+    } else {
+      serachForNote();
+      selectedNote = false;
+      removeElementsByClass();
+      loadDataFromAPI();
+      return;
+    }
+  } else {
+    alert(`input is missing`);
+
+    return;
+  }
+}
+
+function delete_note() {
+  if (selectedNote == true) {
+    let index = allMyNotes.findIndex((el) => {
+      return el.id == selectedID;
+    });
+    allMyNotes.splice(index, 1);
+    saveToLocalStorage();
+    removeElementsByClass();
+    loadDataFromAPI();
+  } else {
+    console.log(`nothing was deleted`);
+  }
+}
+
+function removeElementsByClass() {
+  let elements = document.getElementsByClassName("single-note");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+function create_note_UI(title_message, note_message, timestamp_message, id) {
+  const single_note = document.createElement("div");
+  single_note.classList.add("single-note");
+  single_note.setAttribute("id", id);
+  single_note.setAttribute("onclick", `selectNote(${id})`);
+  const note = document.createElement("div");
+  note.classList.add("note");
+  const title = document.createElement("span");
+  title.classList.add("title");
+  title.setAttribute("data-title-id", id);
+  const text_note = document.createElement("span");
+  text_note.classList.add("text");
+  text_note.setAttribute("data-text-id", id);
+  const br1 = document.createElement("br");
+  const br2 = document.createElement("br");
+  const text = document.createElement("span");
+  text.classList.add("text");
+  const time_stamp = document.createElement("div");
+  time_stamp.classList.add("time-stamp");
+  const time = document.createElement("span");
+  time.classList.add("time");
+
+  let new_title_message = document.createTextNode(title_message);
+  let new_note_message = document.createTextNode(note_message);
+  let new_timestamp_message = document.createTextNode(timestamp_message);
+
+  title.appendChild(new_title_message);
+  text_note.appendChild(new_note_message);
+  time.appendChild(new_timestamp_message);
+
+  single_note.append(note);
+  note.appendChild(title);
+  note.appendChild(br1);
+  note.appendChild(br2);
+  note.appendChild(text_note);
+  single_note.appendChild(time_stamp);
+  time_stamp.appendChild(time);
+
+  let root = document.getElementById("note-list");
+  root.appendChild(single_note);
+}
+
+function selectNote(noteID) {
+  let previousTitle = document.querySelector(
+    `[data-title-id="${noteID}"]`
+  ).innerHTML;
+  let previousText = document.querySelector(
+    `[data-text-id="${noteID}"]`
+  ).innerHTML;
+  document.getElementById("title-input-field").value = previousTitle;
+  document.getElementById("textArea-input-field").value = previousText;
+  selectedNote = true;
+  selectedID = noteID;
+}
+
+function serachForNote() {
+  let index = allMyNotes.findIndex((el) => {
+    return el.id == selectedID;
+  });
+  allMyNotes[index].title = document.getElementById("title-input-field").value;
+  allMyNotes[index].note_message = document.getElementById(
+    "textArea-input-field"
+  ).value;
+  allMyNotes[index].timestamp = Date.now();
+  saveToLocalStorage();
+}
+
+function clearInputFields() {
+  document.getElementById("title-input-field").value = "";
+  document.getElementById("textArea-input-field").value = "";
+  selectedNote = false;
+}
